23c44b00ebcd25e540e994ffae06e8a51b13e473
yuankaikevinyang <yuankai168gm@gmail.com>
2025-04-06 00:46:25 -0700

    started on working on the profile section

--- app/src/Dashboard.jsx
+++ app/src/Dashboard.jsx
@@ -2,9 +2,9 @@ import "./Styles.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import React from "react";
 import NavBar from "./components/NavBar.jsx";
-import HeroSection from "./components/Hero.jsx";
 import Section from "./components/Section";
-import Dropdown from "./components/Dropdown";
+import UserProfile from "./components/UserProfile/index.jsx";
+
 
 function Dashboard() {
   
@@ -13,7 +13,8 @@ function Dashboard() {
   return (
     <>
       <NavBar/>
-      <HeroSection />
+      <UserProfile/>
+
     </>
   );
 }
