d05929f08a698c334257db377681380b2d86aa98
yuankaikevinyang <yuankai168gm@gmail.com>
2025-04-06 01:12:58 -0700

    got image on profile picture

--- app/src/Dashboard.jsx
+++ app/src/Dashboard.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import NavBar from "./components/NavBar.jsx";
 import Section from "./components/Section";
 import UserProfile from "./components/UserProfile/index.jsx";
+import profileImage from "./assets/ryan.jpg";
 
 
 function Dashboard() {
@@ -12,9 +13,12 @@ function Dashboard() {
   
 return (
     <>
-      <NavBar/>
-      <UserProfile name="John Doe" username="jdoe123"/>
-
+      <NavBar />
+      <UserProfile 
+        name="John Doe" 
+        username="jdoe123" 
+        profilePicture={profileImage}
+      />
     </>
 );
 }
