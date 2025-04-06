4577bab6807eb241ef5c76af16b410deede895d4
Jonathan Ty <jonathanty42@gmail.com>
2025-04-06 11:31:51 -0700

    Remove deprecated react_jsx-runtime and its source map files; update main.jsx for consistent import style and formatting.

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import StudyPack from './StudyPack.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './Home.jsx'
-import Dashboard from './Dashboard.jsx'
-import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Home.jsx";
+import Dashboard from "./Dashboard.jsx";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App/>
-  </StrictMode>,
+    <App />
+  </StrictMode>
 );
