78d8e8b4f4bef5efa27b3663896cc1e77dd3307c
Jonathan Ty <jonathanty42@gmail.com>
2025-04-06 03:16:11 -0700

    Add Bootstrap and Bootstrap Icons; refactor NavBar and Flashcard components

---
+++ app/src/Flashcard.jsx
@@ -0,0 +1,18 @@
+import "./Styles.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import React from "react";
+import NavBar from "./components/NavBar.jsx";
+import Section from "./components/Section.jsx";
+import UserProfile from "./components/UserProfile/index.jsx";
+
+
+function Flashcard() {
+return (
+    <>
+      <NavBar/>
+      
+    </>
+);
+}
+
+export default Flashcard;
