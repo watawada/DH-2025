f317ae4ed86ef5ec1ee24abc6cf5293761567516
yuankaikevinyang <yuankai168gm@gmail.com>
2025-04-06 01:02:35 -0700

    fixed dashboard

--- app/src/Dashboard.jsx
+++ app/src/Dashboard.jsx
@@ -19,21 +19,4 @@ return (
 );
 }
 
-export default Dashboardimport "./Styles.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import React from "react";
-import NavBar from "./components/NavBar.jsx";
-import HeroSection from "./components/Hero.jsx";
-import Section from "./components/Section";
-import Dropdown from "./components/Dropdown";
-
-function Dashboard() {
-  return (
-    <>
-      <NavBar />
-      <HeroSection />
-    </>
-  );
-}
-
-export default App;
+export default Dashboard;
